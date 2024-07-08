fb30c88a2fe9b488f592e5f7c26648af31ca56df
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-07-08 14:05:44 -0600

    add create superuser and more validation to user manager

--- app/core/models.py
+++ app/core/models.py
@@ -13,8 +13,23 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create, superuser."""
+        if not password:
+            raise ValueError('Superuser must have a password.')
+
+        user = self.create_user(email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,3 +34,20 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser('test_user@example.com', 'test123')
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_new_superuser_without_password_raises_error(self):
+        """Test that creating a superuser without a passwrod raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_superuser('test_user@example.com', '')
+
