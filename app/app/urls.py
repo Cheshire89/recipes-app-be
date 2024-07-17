bdacd87b5cec4f95113475e4884f99ba5db778b4
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-07-17 11:12:00 -0600

    image storage setup

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,14 +19,24 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView
 )
+from django.conf.urls.static import static
+from django.conf import settings
+
+schema_name = 'api-schema'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/scema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/schema/', SpectacularAPIView.as_view(), name=schema_name),
     path(
         'api/docs',
-        SpectacularSwaggerView.as_view(url_name='api-schema')
+        SpectacularSwaggerView.as_view(url_name=schema_name)
     ),
     path('api/user/', include('user.urls')),
     path('api/recepie/', include('recipe.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
