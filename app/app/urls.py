04b83b6c82b466c3e25c556ad5b2f7f349e087e7
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-07-30 23:45:13 -0600

    added health check endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,11 +21,13 @@ from drf_spectacular.views import (
 )
 from django.conf.urls.static import static
 from django.conf import settings
+from core import views as core_views
 
 schema_name = 'api-schema'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health-check/', core_views.health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(), name=schema_name),
     path(
         'api/docs',
